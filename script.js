5718661b3afaa60e0ae130463033241e5ac26d7f
Alba Velasco <albavelascoglez@gmail.com>
2023-06-29 13:44:14 +0200

    done

--- script.js
+++ script.js
@@ -1,50 +1,114 @@
-
 const main = document.querySelector("#main");
 const form = document.querySelector("#form");
 const search = document.querySelector("#search");
 
-
-
-search.addEventListener('keydown', (event)=>{
+search.addEventListener("keydown", (event) => {
   let searchValue = search.value;
-  let emptyValue = ''; 
-  const enterCode = 'Enter';
+  let emptyValue = "";
+  const enterCode = "Enter";
 
-  if((event.code === enterCode) && (searchValue !== emptyValue)){
-    event.preventDefault()
-    getUser(searchValue)
+  if (event.code === enterCode && searchValue !== emptyValue) {
+    event.preventDefault();
+    getUser(searchValue);
+    getUserRepos(searchValue);
   }
-})
+});
 
+function getUser(userName) {
+  const APIURL = `https://api.github.com/users/${userName}`;
+  axios
+    .get(APIURL)
+    .then(async (response) => {
+      const avatar = response.data.avatar_url;
+      const name = response.data.name;
+      const bio = response.data.bio;
+      const followers = response.data.followers;
+      const following = response.data.following;
+      const reposNumber = response.data.public_repos;
 
-
-function getUser(userName){
-
-  const APIURL = `https://api.github.com/users/${userName}`
-    axios.get(APIURL)
-    .then((response) => {
-      console.log(response.data);
-      const avatar = response.data.avatar_url
-      const name = response.data.name
-      const followers = response.data.followers
-      const repos = response.data.repos_url
-      createCard({avatar: avatar,name: name, followers: followers, repos: repos})
+      const reposUrl = response.data.repos_url;
+      const reposResponse = await getUserRepos(reposUrl);
+      const repos = reposResponse.data;
+      createCard({
+        avatar: avatar,
+        name: name,
+        bio: bio,
+        followers: followers,
+        following: following,
+        reposNumber: reposNumber,
+        repos: repos,
+      });
     })
-    .catch(err =>{
-        console.log(err)
-    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-function createCard({avatar, name, repos}){ 
-  let card = document.createElement('div')
-  card.setAttribute('class', 'card')
-  let avatarImg = document.createElement('img')
-  avatarImg.setAttribute('class', 'avatar')
-  avatarImg.setAttribute('src', avatar)
-  let textCard = document.createElement('h2')
-  textCard.innerText = name
-  
-  card.appendChild(avatarImg)
-  card.appendChild(textCard)
-  main.appendChild(card)
+async function getUserRepos(url) {
+  return axios.get(url);
+}
+
+function createCard({
+  avatar,
+  name,
+  bio,
+  followers,
+  following,
+  reposNumber,
+  repos,
+}) {
+  // card
+  let card = document.createElement("div");
+  card.setAttribute("class", "card");
+
+  // avatar
+  let avatarImg = document.createElement("img");
+  avatarImg.setAttribute("class", "avatar");
+  avatarImg.setAttribute("src", avatar);
+
+  // userInfo
+  let userInfo = document.createElement("div");
+  userInfo.setAttribute("class", "user-info");
+
+  // name h2
+  let textCard = document.createElement("h2");
+  textCard.innerText = name;
+
+  // bio
+  let bioText = document.createElement("p");
+  bioText.innerText = bio;
+
+  // followers list
+  let followerList = document.createElement("ul");
+  let followersNumber = document.createElement("li");
+  followersNumber.innerText = `${followers} Followers`;
+  let followingNumber = document.createElement("li");
+  followingNumber.innerHTML = `${following} Following`;
+  let reposNumbers = document.createElement("li");
+  reposNumbers.innerText = `${reposNumber} Repos`;
+
+  // repos list
+  let reposList = document.createElement("ul");
+
+  const splitRepos = repos.splice(0, 5);
+  splitRepos.forEach((repo) => {
+    const repoName = repo.name;
+    const nameLi = document.createElement("li");
+    nameLi.setAttribute("class", "repo");
+    nameLi.innerText = repoName;
+    reposList.appendChild(nameLi);
+  });
+
+  // append elements
+  card.appendChild(avatarImg);
+  card.appendChild(userInfo);
+  userInfo.appendChild(textCard);
+  userInfo.appendChild(bioText);
+  userInfo.appendChild(followerList);
+  followerList.appendChild(followersNumber);
+  followerList.appendChild(followingNumber);
+  followerList.appendChild(reposNumbers);
+
+  userInfo.appendChild(reposList);
+  main.appendChild(card);
 }
